60984346ea5849cd53667f70163ba821d13acb99
y.imuta <yujiimuta0572@gmail.com>
2020-09-23 18:11:17 +0900

    commit

---
+++ src/P_189.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cvxpy as cp
+
+x1, x2 =  cp.Variable(), cp.Variable()
+obj = cp.Maximize(2*x1 + x2)
+cons = [
+    x1 - 0.5*x2 >= 1,
+    x1 - x2 <= 2,
+    x1 + x2 <= 4,
+    x1 >= 0,
+    x2 >= 0
+]
+
+
+P = cp.Problem(obj, cons)
+P.solve(verbose=True)
+print("最適値")
+print(P.value)
+print("最適解")
+print(x1.value, x2.value)
